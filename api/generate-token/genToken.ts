93e49f788316536b20b525365192681552be49d5
Destiny911gtr <dhanush911gtr@gmail.com>
2025-05-07 00:37:22 +0530

    New auth method

--- api/generate-token/genToken.ts
+++ api/generate-token/genToken.ts
@@ -1,14 +1,18 @@
 import { apiClient } from "../client/ky-instance";
 import { genTokenResponse } from "./types";
-
-const username = process.env.EXPO_PUBLIC_USERNAME || "";
-const password = process.env.EXPO_PUBLIC_PASSWORD || "";
+import DeviceInfo from "react-native-device-info";
 
 export async function genToken(): Promise<genTokenResponse> {
-  const response = await apiClient
-    .post("auth/login", {
-      json: { username, password },
-    })
-    .json<genTokenResponse>();
-  return response;
+  try {
+    const instanceId = await DeviceInfo.getInstanceId();
+    const response = await apiClient
+      .post("auth/login", {
+        json: { device_id: instanceId },
+      })
+      .json<genTokenResponse>();
+    return response;
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
 }
