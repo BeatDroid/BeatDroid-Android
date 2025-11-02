b8ca917ad4dcd4086fd9a2f04cf1da3f4492bcbd
Destiny911gtr <dhanush911gtr@gmail.com>
2025-11-03 01:13:36 +0530

    Refactor | Extract header component and adjust list header integration in search history tabs

--- app/(home-tabs)/search-history.tsx
+++ app/(home-tabs)/search-history.tsx
@@ -414,13 +414,19 @@ export default function SearchHistoryView() {
     });
   }, [trackHistory.length, albumHistory.length]);
 
-  return (
-    <Background disableSafeArea className={"pt-safe px-0"}>
+  const Header = React.useCallback(
+    () => (
       <AnimatedHeader
         title="Search History 📜"
         description="Tracks and albums you couldn't get enough of"
-        containerClassName={"px-5"}
+        containerClassName={"px-5 pb-4"}
       />
+    ),
+    [],
+  );
+
+  return (
+    <Background disableSafeArea className={"pt-safe px-0"}>
       <Tabs
         value={currentTab}
         onValueChange={setCurrentTab}
@@ -437,13 +443,14 @@ export default function SearchHistoryView() {
               keyExtractor={(item) => item.id.toString()}
               onEndReached={() => incrementallyLoadAlbums({})}
               onEndReachedThreshold={0.7}
-              contentContainerClassName={"mx-0 pb-[70] pt-4"}
+              contentContainerClassName={"mx-0 pb-[70]"}
               renderItem={renderItem}
               showsVerticalScrollIndicator={false}
               fadingEdgeLength={80}
               scrollEventThrottle={16}
               refreshing={refreshingAlbums}
               onRefresh={refreshAlbums}
+              ListHeaderComponent={Header}
               refreshControl={
                 <TwRefreshControl
                   className="bg-muted text-primary"
@@ -465,13 +472,14 @@ export default function SearchHistoryView() {
               keyExtractor={(item) => item.id.toString()}
               onEndReached={() => incrementallyLoadTracks({})}
               onEndReachedThreshold={0.7}
-              contentContainerClassName={"mx-0 pb-[70] pt-4"}
+              contentContainerClassName={"mx-0 pb-[70]"}
               renderItem={renderItem}
               showsVerticalScrollIndicator={false}
               fadingEdgeLength={80}
               scrollEventThrottle={16}
               refreshing={refreshingTracks}
               onRefresh={refreshTracks}
+              ListHeaderComponent={Header}
               refreshControl={
                 <TwRefreshControl
                   className="bg-muted text-foreground"
